a8bf0ec618e24915922dfaece3e3db4a8eb41e8f
devmev10 <ravitokas90@gmail.com>
2023-01-23 11:50:30 +0530

    Add style to input

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,12 +1,34 @@
-import { TextInput, View } from "react-native";
+import { TextInput, View, StyleSheet } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 export default function StartGameScreen() {
   return (
-    <View>
-      <TextInput />
+    <View style={styles.inputContainer}>
+      <TextInput style={styles.numberInput} maxLength={2} />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+    elevation: 4,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb42f",
+    borderBottomWidth: 2,
+    color: "#ddb42f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
