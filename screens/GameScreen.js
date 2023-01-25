e1240952678fd6afdd785e1f95f59b8c5d80a205
devmev10 <ravitokas90@gmail.com>
2023-01-25 09:52:01 +0530

    Add safe area view to take care of Notch

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,21 @@
-import { Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function GameScreen() {
-  return <Text>Game Screen</Text>;
+  return (
+    <View style={styles.screen}>
+      <Text>Opponents's Guess</Text>
+      GUESS
+      <View>
+        <Text>Higher or Lower?</Text>+ -
+      </View>
+      <View>LOG ROUNDS</View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 24,
+  },
+});
